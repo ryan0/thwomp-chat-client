74b4499f515aafe768f63a88abd2b89b6ed8040c
Ryan Miller <ryan@rmiller.dev>
2024-11-04 00:46:04 -0500

    update prod websocket url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   loginUrl: 'https://thwompchat.com/login',
   logoutUrl: 'https://thwompchat.com/logout',
-  wsBrokerURL: 'ws://localhost:8080/websocket'
+  wsBrokerURL: 'ws://thwompchat.com/websocket'
 };
